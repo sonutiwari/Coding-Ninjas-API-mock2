a396b569bf6e636ea562f7d8140036b2543764ff
Sonu Tiwari <ankorha@gmail.com>
2020-03-30 22:54:58 +0530

    added auth for some rroutes

--- routers/v1/reports.route.js
+++ routers/v1/reports.route.js
@@ -1,5 +1,9 @@
 let express       = require('express');
+
+// auth
+let auth = require('../utilities/auth');
+
 let reportsRouter     = express.Router();
 let reportsController = require('../../controllers/reports.controller');
-reportsRouter.get('/:status/', reportsController.getAllReportOfStatus);
+reportsRouter.get('/:status/', auth, reportsController.getAllReportOfStatus);
 module.exports = reportsRouter;
